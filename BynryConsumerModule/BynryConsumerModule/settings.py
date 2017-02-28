f7ae902287dfe39efe429b81f52370ee835a59fe
Vikas <vikas@byrny.com>
2017-02-28 15:29:15 +0530

    settings

--- BynryConsumerModule/BynryConsumerModule/settings.py
+++ BynryConsumerModule/BynryConsumerModule/settings.py
@@ -62,16 +62,23 @@ WSGI_APPLICATION = 'BynryConsumerModule.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-   'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'BynryConsumer_db',
-            'USER': 'root',
-            'PASSWORD': 'root',
-            'HOST': '127.0.0.1',
-            'PORT': '3306',
-            'TIME_ZONE': 'Asia/Kolkata',
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
+
+# DATABASES = {
+#    'default': {
+#             'ENGINE': 'django.db.backends.mysql',
+#             'NAME': 'BynryConsumer_db',
+#             'USER': 'root',
+#             'PASSWORD': 'root',
+#             'HOST': '127.0.0.1',
+#             'PORT': '3306',
+#             'TIME_ZONE': 'Asia/Kolkata',
+#    }
+# }
 
 
 # Internationalization
