026d784f8757d412d0ffb18a3a4c20b99a66d398
Vikas <vikas@byrny.com>
2017-03-07 11:42:11 +0530

    complaint

--- BynryConsumerModule/BynryConsumerModuleapp/migrations/0001_initial.py
+++ BynryConsumerModule/BynryConsumerModuleapp/migrations/0001_initial.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import migrations, models
+from django.db import models, migrations
 import django.contrib.auth.models
 import django.utils.timezone
 from django.conf import settings
@@ -57,6 +57,19 @@ class Migration(migrations.Migration):
                 ('created_on', models.DateTimeField(default=django.utils.timezone.now)),
                 ('updated_on', models.DateTimeField(null=True, blank=True)),
                 ('is_deleted', models.BooleanField(default=False, choices=[(True, True), (False, False)])),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Pincode',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('pincode', models.CharField(default=None, max_length=500)),
+                ('created_by', models.CharField(max_length=50)),
+                ('updated_by', models.CharField(max_length=50, null=True, blank=True)),
+                ('created_on', models.DateTimeField(default=django.utils.timezone.now)),
+                ('updated_on', models.DateTimeField(null=True, blank=True)),
+                ('is_deleted', models.BooleanField(default=False, choices=[(True, True), (False, False)])),
+                ('city', models.ForeignKey(blank=True, to='BynryConsumerModuleapp.City', null=True)),
             ],
         ),
         migrations.CreateModel(
