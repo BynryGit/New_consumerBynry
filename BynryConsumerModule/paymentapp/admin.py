d149edaf5f871f636cf1127f4bb9a80266687664
Shubham S <finance@tungstenbigdata.com>
2017-03-09 13:01:52 +0530

    ss

--- BynryConsumerModule/paymentapp/admin.py
+++ BynryConsumerModule/paymentapp/admin.py
@@ -3,8 +3,8 @@ import models
 # Register your models here.
 
 class PaymentDetailAdmin(admin.ModelAdmin):
-    list_display = ('id', 'transaction_id', 'consumer_id', 'bill_month', 'unit_consumed', 'bill_amount_paid', 'payment_date', 'payment_by')
+    list_display = ('id', 'transaction_id', 'consumer_id', 'bill_amount_paid', 'payment_date', 'payment_by')
     search_fields = ('id', 'transaction_id', 'consumer_id')
-    list_filter = ('bill_month', 'payment_by', 'is_deleted')
+    list_filter = ('payment_by', 'is_deleted')
 
 admin.site.register(models.PaymentDetail,PaymentDetailAdmin)
