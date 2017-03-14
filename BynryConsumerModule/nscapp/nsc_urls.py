e1e29b56d739ca6880f180cd3a62f2e48bf61096
Shubham S <finance@tungstenbigdata.com>
2017-03-14 10:48:28 +0530

    ss

---
+++ BynryConsumerModule/nscapp/nsc_urls.py
@@ -0,0 +1,9 @@
+from nscapp import views
+from django.conf.urls import include, url
+
+
+urlpatterns = [
+#SHUBHAM NEW
+    url(r'^new-connection-list/', views.new_connection_list,name='new_connection'),    
+    url(r'^add-new-consumer/', views.add_new_consumer,name='add_new_consumer'),        
+]
