2477d28169f24a33043c7a234ea982378c768990
Swapnil <swapnil.kadu@tungstenbigdata.com>
2017-03-14 11:00:52 +0530

    nscapp

--- BynryConsumerModule/nscapp/nsc_urls.py
+++ BynryConsumerModule/nscapp/nsc_urls.py
@@ -4,6 +4,6 @@ from django.conf.urls import include, url
 
 urlpatterns = [
 #SHUBHAM NEW
-    url(r'^new-connection-list/', views.new_connection_list,name='new_connection'),    
+    url(r'^$', views.new_connection_list,name='new_connection'),    
     url(r'^add-new-consumer/', views.add_new_consumer,name='add_new_consumer'),        
 ]
