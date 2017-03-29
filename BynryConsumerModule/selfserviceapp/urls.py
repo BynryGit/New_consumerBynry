1ad6095a8ef703251b7b84f56ac8827f26a45b6c
Swapnil <swapnil.kadu@tungstenbigdata.com>
2017-03-29 10:18:44 +0530

    update

--- BynryConsumerModule/selfserviceapp/urls.py
+++ BynryConsumerModule/selfserviceapp/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
 	url(r'^$', views.login, name='login'),
     url(r'^index/', views.home_screen, name='home_screen'),
     url(r'^register-new-user/', views.register_new_user, name='register_new_user'),
-    #url(r'^my-bills/', views.my_bills, name='my_bills'),
+    url(r'^my-bills/', views.my_bills, name='my_bills'),
+    url(r'^manage-accounts/', views.manage_accounts, name='manage_accounts'),
+    url(r'^add-new-account/', views.add_new_account, name='add_new_account'),
+    url(r'^complaints/', views.complaints, name='complaints'),
+    url(r'^services/', views.services, name='services'),
 ]
