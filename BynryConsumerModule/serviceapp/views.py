d7c760edebbc223530b207ea42fc78e2d9c86752
Shubham S <finance@tungstenbigdata.com>
2017-09-11 16:16:07 +0530

    ss

--- BynryConsumerModule/serviceapp/views.py
+++ BynryConsumerModule/serviceapp/views.py
@@ -93,7 +93,7 @@ def get_service_data(request):
             'consumer_no' : '11111111111',
             'consumer_name' : 'ASD',
             'service_source' : 'Mobile',
-            'service_status' : '<a data-toggle="modal" data-target="#assign_job">Assigned</a>',
+            'service_status' : '<a data-toggle="modal" data-target="#assign_job">Open</a>',
 
         } 
         service_list.append(service_data)
